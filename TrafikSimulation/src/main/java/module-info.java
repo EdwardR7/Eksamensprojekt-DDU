ed36caa5a9391afa4f5ea30d5403a0a8b7d2aeb8
EdwardR7 <36159443+EdwardR7@users.noreply.github.com>
2022-03-22 13:14:01 +0100

    ,

--- TrafikSimulation/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.openjfx.trafiksimulation {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens org.openjfx.trafiksimulation to javafx.fxml;
-    exports org.openjfx.trafiksimulation;
-}
